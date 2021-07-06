3671ca892bed0b6343d0fae75fc347476281983b
jisse Reitsma <jisse@yireo.com>
2021-07-06 14:24:38 +0200

    Adding all files to git via Yireo Command

---
+++ src/Storefront/Controller/ExampleJsonController.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace SwagTraining\JsonController\Storefront\Controller;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\Framework\Routing\Annotation\Since;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @RouteScope(scopes={"storefront"})
+ */
+class ExampleJsonController extends StorefrontController
+{
+    private SystemConfigService $systemConfigService;
+
+    /**
+     * ExampleJsonController constructor.
+     * @param SystemConfigService $systemConfigService
+     */
+    public function __construct(SystemConfigService $systemConfigService){
+        $this->systemConfigService = $systemConfigService;
+    }
+
+    /**
+     * @Since("6.4.0.0")
+     * @Route("/example/json", name="frontend.swag-training.json", methods={"GET"}, defaults={"XmlHttpRequest"=true})
+     */
+    public function getData(Request $request, Context $context): JsonResponse
+    {
+        $instance = $this->systemConfigService->get('SwagTrainingPluginConfig.config.instance');
+        return new JsonResponse([
+            'instance' => $instance
+        ]);
+    }
+}
---
+++ src/SwagTrainingJsonController.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace SwagTraining\JsonController;
+
+use Shopware\Core\Framework\Plugin;
+
+class SwagTrainingJsonController extends Plugin
+{
+}
